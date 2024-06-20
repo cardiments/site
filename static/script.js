9a1467f0c83b5bb3b30956a2e6987960d2e41b9d
joel <joseph.leach@towerhamlets.org.uk>
2024-06-20 22:36:21 +0100

    nav cards

--- static/script.js
+++ static/script.js
@@ -1,1 +1,19 @@
-console.log("Hello 🌎");
+console.log("🂡 greetings from cardiments 🂡")
+
+const suits = document.querySelectorAll("img.nav") // Get the button from the page
+if (suits) { // Detect clicks on the button
+  [...suits].map(suit => {
+    suit.onclick = function () {
+      // The 'dipped' class in style.css changes the appearance on click
+      suit.classList.toggle("dipped")
+    }
+    suit.onmouseover = function () {
+      // The 'dipped' class in style.css changes the appearance on click
+      suit.classList.toggle("dipped")
+    }
+    suit.onmouseout = function () {
+      // The 'dipped' class in style.css changes the appearance on click
+      suit.classList.toggle("dipped")
+    }
+  })
+}
